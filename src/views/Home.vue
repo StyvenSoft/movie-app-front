a0fe50fb95c033409f7ddc27081dcc71611962b7
Styvensoft <steveen.echeverri@gmail.com>
2021-12-18 17:02:12 -0500

    Created header and MovieDatails

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="featured-card">
+      <router-link to="/movie/tt0409591">
+        <img src="https://www.atomcreativemedia.com/wp-content/uploads/2018/09/orange-and-blue-movies-2.jpg" alt="Font poster" class="featured-img" />
+        <dic class="detail">
+          <h3>More movies</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi
+            voluptatibus, omnis est perferendis earum vero atque ex eius,
+            officia facilis veniam, ad architecto. Nihil ea expedita maiores
+            dolores tempora quia!
+          </p>
+        </dic>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+export default {};
+</script>
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+<style lang="scss">
+.home{
+  .featured-card{
+    position: relative;
+
+    .featured-img{
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail{
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgb(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3{
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p{
+        color: #fff;
+
+      }
+    }
   }
 }
-</script>
+</style>
