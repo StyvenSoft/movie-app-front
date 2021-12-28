7266e1823c276f77265d91d6dbcd66228c0bfcce
Styvensoft <steveen.echeverri@gmail.com>
2021-12-27 21:56:26 -0500

    Created fetch api movies details

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,17 +19,59 @@
       </router-link>
     </div>
 
-    <form @submit.prevent="" class="search-box">
-      <input type="text" placeholder="What are you looking for?" />
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input
+        type="text"
+        placeholder="What are you looking for?"
+        v-model="search"
+      />
       <input type="submit" value="Search" />
     </form>
 
-    <div class="movie-list">MOVIES</div>
+    <div class="movie-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import env from '../../env.js';
+
+export default {
+  setup() {
+    const search = ref("");
+    const movies = ref([]);
+
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`https://www.omdbapi.com/?apikey=${env.apiKey}&s=${search.value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            movies.value = data.Search;
+            search.value = "";
+          });
+      }
+    };
+
+    return {
+      search,
+      movies,
+      SearchMovies,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -110,8 +152,66 @@ export default {};
         text-transform: uppercase;
         transition: 0.4s;
 
-        &:active{
+        &:active {
           background-color: #3b8070;
+        }
+      }
+    }
+  }
+
+  .movie-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 8px;
+
+    .movie {
+      min-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42B883;
+            color: #fff;
+            bottom: 16px;
+            left: 0;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496883;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0 0 8px 8px;
+
+          .year {
+            color: #aaa;
+            font-size: 14px;
+          }
+
+          h3 {
+            color: #fff;
+            font-weight: 600;
+            font-size: 17px;
+          }
         }
       }
     }
